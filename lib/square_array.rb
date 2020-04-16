b5eedf52fbe9eee4123595475de5d21ca09b37af
Caroline Glass <carolineglass89@gmail.com>
2020-04-16 14:30:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
-def square_array(array)
-  new_array = []
+def square_array(numbers)
+  new_numbers_squared = []
   counter = 0
   while counter < array.length do 
-    new_array.push(array[counter] * array[counter])
+    new_numbers_squared.push(numbers[counter] * numbers[counter])
   end 
-  new_array
+  new_numbers_squared
 end
 
