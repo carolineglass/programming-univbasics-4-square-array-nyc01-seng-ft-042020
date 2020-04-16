445bda2850baea4b63111843147d181564f6c66d
Caroline Glass <carolineglass89@gmail.com>
2020-04-16 14:28:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter < array.length do 
-    new_array.push(array[counter] ** 2)
+    new_array.push(array[counter] * array[counter])
   end 
   new_array
 end
